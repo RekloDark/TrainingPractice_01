75ef48bddae0df85647c8c75cd7b11dae725a343
RekloDark <73885784+RekloDark@users.noreply.github.com>
2020-11-06 16:31:12 +0300

    Add files via upload

---
+++ KNV_Task_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KNV_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            do
+            {
+
+                str = Console.ReadLine();
+
+            } while (!str.Equals("exit"));
+        }
+    }
+}
